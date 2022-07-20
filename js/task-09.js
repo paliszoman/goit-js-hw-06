c4c431720b27a61e9baf6ce3baf30b630c6a8844
paliszoman <kamilkrolik@hotmail.com>
2022-07-20 23:50:49 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,9 +2,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let changeColor = () => { ain.style.backgroundColor = getRandomHexColor(); }
+let changeColor = () => { main.style.backgroundColor = getRandomHexColor(); }
 
-let button = document.querySelector('.change-color')
+let button = document.querySelector('.change-color');
 let main = document.querySelector('body');
-
 button.addEventListener('click', changeColor);
